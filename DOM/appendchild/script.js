aaf9ad0a0af87e32af74d395521041cd35afc537
Kaustubh-trivedi <kaustubh@alobhadev.com>
2026-02-27 12:16:14 +0530

    appendchild

--- DOM/appendchild/script.js
+++ DOM/appendchild/script.js
@@ -1,3 +1,9 @@
 const h1 = document.querySelector('h1');
 const container = document.querySelector('.container');
 const card = document.querySelector('.card');
+
+//appendChild   ---> Cut and Paste
+container.appendChild(h1); //  -> This will take h1 and paste inside container, it is like cut and paste;
+
+// cloneNode  then appendChild      ---> Copy and paste
+container.appendChild(h1.cloneNode())
